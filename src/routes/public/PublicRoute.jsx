05833bdfab6ec049b764245fc7e130ca073467c0
Emmanuel Gonzalez <jgonzalez13@ucol.mx>
2020-06-06 13:35:47 -0500

    navess to naves

--- src/routes/public/PublicRoute.jsx
+++ src/routes/public/PublicRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 const PublicRoute = ({ component: Component, auth, ...rest }) => (
-  <Route {...rest} render={(props) => (!auth ? <Component {...props} /> : <Redirect to="/navess" />)} />
+  <Route {...rest} render={(props) => (!auth ? <Component {...props} /> : <Redirect to="/naves" />)} />
 );
 
 export default PublicRoute;
