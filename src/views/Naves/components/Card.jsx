ca511b0d907b732e592f391c4d6a8bb547175403
Heriberto Figueroa Michel <hfigueroa@ucol.mx>
2020-06-06 17:23:44 -0500

    cartas 2

--- src/views/Naves/components/Card.jsx
+++ src/views/Naves/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import './Card.css';
 
 const MyCard = ({ nave }) => {
   const history = useHistory();
-  const { nombreNave, descripcionNave, idNave } = nave;
+  const { nombreNave, descripcionNave, idNave, rutaNave } = nave;
   const imgRandom = `https://source.unsplash.com/1600x900/?nature,water`;
 
   function toNaveView() {
@@ -12,14 +13,27 @@ const MyCard = ({ nave }) => {
   }
 
   return (
-    <Card className="mr-2">
+    <Card className="mr-2 bg-light border-light card-shadow ">
       <CardImg top width="100%" src={imgRandom} alt="Card image cap" />
       <CardBody>
-        <CardTitle>{nombreNave}</CardTitle>
-        <CardSubtitle>{idNave}</CardSubtitle>
-        <CardSubtitle>{descripcionNave}</CardSubtitle>
+        <div>
+          <CardTitle className="h5 ">Nave: {nombreNave}</CardTitle>
+        </div>
+        <CardSubtitle>ID: {idNave}</CardSubtitle>
+        <br />
+        <CardSubtitle className="border-car">
+          Descripcion:
+          <br /> {descripcionNave}
+        </CardSubtitle>
+        <CardSubtitle className="p">
+          <br />
+          Ubicacion: <br />
+          {rutaNave}
+        </CardSubtitle>
         <CardText></CardText>
-        <Button onClick={toNaveView}>Button</Button>
+        <Button onClick={toNaveView} className="btn-block" color="info">
+          DATOS
+        </Button>
       </CardBody>
     </Card>
   );
